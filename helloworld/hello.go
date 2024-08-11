b2b9c79596c725045ee2771fa17420ad789493e0
David Ortega <davidortegafarrerons@gmail.com>
2024-08-11 22:49:52 +0200

    refactor Hello to have a switch case so it can return the hello prefix depending on the given language, create more tests for it

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,10 +1,24 @@
 package main
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
 }
--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -4,18 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people with name parameter", func(t *testing.T) {
-		got := Hello("David")
+		got := Hello("David", "")
 		want := "Hello, David"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Saying 'Hello World' when parameter name is empty", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("David", "Spanish")
+		want := "Hola, David"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("David", "French")
+		want := "Bonjour, David"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {

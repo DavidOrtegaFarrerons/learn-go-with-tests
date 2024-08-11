88ed8b18d59f1a4e13b1e361196d557a6bd8ccb2
David Ortega <davidortegafarrerons@gmail.com>
2024-08-11 22:53:09 +0200

    split function into two different functions to sepaarte the logic of the prefix from the one of saying hello

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -11,14 +11,18 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name
+	return
 }

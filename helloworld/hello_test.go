32971196b5a1e8641621eb9a939619ed45e2206b
David Ortega <davidortegafarrerons@gmail.com>
2024-08-11 22:43:49 +0200

    Add new helper method for the test suite

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -3,9 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("David")
-	want := "Hello, David"
+	t.Run("Saying hello to people with name parameter", func(t *testing.T) {
+		got := Hello("David")
+		want := "Hello, David"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Saying 'Hello World' when parameter name is empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	//Used to tell the test suite that this method is a helper,
+	// so when it fails the line will be the function call rather than this helper
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}

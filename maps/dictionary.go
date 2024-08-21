481508a58fcdca02cdd81853c080c7946e535aad
David Ortega Farrerons <davidortegafarrerons@gmail.com>
2024-08-21 18:07:24 +0200

    Create CRUD like operations with dictionary

---
+++ maps/dictionary.go
@@ -0,0 +1,60 @@
+package maps
+
+type Dictionary map[string]string
+
+var (
+	ErrorNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrorWordExists       = DictionaryErr("the word already exists in the dictionary")
+	ErrorWordDoesNotExist = DictionaryErr("the word does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrorNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	case nil:
+		return ErrorWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		return ErrorWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
